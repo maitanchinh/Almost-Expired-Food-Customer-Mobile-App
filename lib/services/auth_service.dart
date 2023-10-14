4c0b36d64646b770c7092276e780d72c4dcc2430
Janglee <votantai4899@gmail.com>
2023-10-14 19:58:22 +0700

    uncomment google auth

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -6,11 +6,12 @@ class AuthService {
   signInWithGoogle() async {
     final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
     final GoogleSignInAuthentication gAuth = await gUser!.authentication;
-    // final credential = GoogleAuthProvider.credential(
-    //   accessToken: gAuth.accessToken,
-    //   idToken: gAuth.idToken
-    // );
-    // return await FirebaseAuth.instance.signInWithCredential(credential);
+    final credential = GoogleAuthProvider.credential(
+      accessToken: gAuth.accessToken,
+      idToken: gAuth.idToken
+    );
+    print(credential.idToken);
+    return await FirebaseAuth.instance.signInWithCredential(credential);
     return gAuth.idToken;
   }
 }
