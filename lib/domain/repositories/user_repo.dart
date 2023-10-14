bcd488dc6c784373068f13a09974d20cc33830c0
Janglee <votantai4899@gmail.com>
2023-10-14 20:45:59 +0700

    auth fix

--- lib/domain/repositories/user_repo.dart
+++ lib/domain/repositories/user_repo.dart
@@ -3,24 +3,44 @@ import 'package:appetit/utils/get_it.dart';
 import 'package:appetit/utils/messages.dart';
 import 'package:dio/dio.dart';
 import 'package:nb_utils/nb_utils.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class UserRepo{
   final Dio apiClient = getIt.get<Dio>();
 
   Future<void> login({required String idToken}) async{
+    print(idToken);
      try {
-       var res = await apiClient.post('/api/auth/google/customer', data: {'idToken': idToken});
-       print(res);
-       var accessToken = res.data['accessToken'] as String?;
-       if (accessToken == null || accessToken.isEmpty) {
-         throw Exception(msg_login_token_invalid);
-       }
-       print('Access Token: ' + accessToken);
-       await setValue(TOKEN_KEY, accessToken);
+
+       var requestBody = {'idToken': idToken};
+       var response = await http.post(
+         Uri.parse('https://expiredfood.azurewebsites.net/api/auth/google/customer'),
+         body: jsonEncode(requestBody),
+         headers: {
+           'Content-Type': 'application/json',
+           // Add any additional headers if needed
+         },
+       );
+
+       print(response.body);
+
+       // await apiClient.post('/api/auth/google/customer', data: {'idToken': idToken});
+       //
+       //
+       // print("called");
+       // var accessToken = res.data['accessToken'] as String?;
+       // if (accessToken == null || accessToken.isEmpty) {
+       //   throw Exception(msg_login_token_invalid);
+       // }
+       // print('Access Token: ' + accessToken);
+       // await setValue(TOKEN_KEY, accessToken);
      } on DioException catch (e) {
        if (e.response!.statusCode == 401) {
+         print(e.stackTrace);
          throw Exception(msg_login_by_google_failed_title);
        }
+       print(e.message);
        throw Exception(msg_server_error);
      }
   }
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -4,14 +4,34 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
   signInWithGoogle() async {
-    final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
-    final GoogleSignInAuthentication gAuth = await gUser!.authentication;
-    final credential = GoogleAuthProvider.credential(
-      accessToken: gAuth.accessToken,
-      idToken: gAuth.idToken
-    );
-    print(credential.idToken);
-    return await FirebaseAuth.instance.signInWithCredential(credential);
-    return gAuth.idToken;
+    // final GoogleSignInAccount? gUser = await GoogleSignIn().signIn();
+    // final GoogleSignInAuthentication gAuth = await gUser!.authentication;
+    // final credential = GoogleAuthProvider.credential(
+    //   accessToken: gAuth.accessToken,
+    //   idToken: gAuth.idToken
+    // );
+    // return await FirebaseAuth.instance.signInWithCredential(credential);
+    // return gAuth.idToken;
+
+    final GoogleSignInAccount? googleSignInAccount =
+    await GoogleSignIn().signIn();
+
+    if (googleSignInAccount != null) {
+      final GoogleSignInAuthentication
+      googleSignInAuthentication =
+      await googleSignInAccount.authentication;
+      final AuthCredential credential =
+      GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+      final UserCredential userCredential = await FirebaseAuth
+          .instance
+          .signInWithCredential(credential);
+
+      final User user = userCredential.user!;
+      final String? idToken = await user.getIdToken();
+      return idToken;
+    }
   }
 }
